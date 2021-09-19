46055af13e51418131644d1abc5192c2293f7275
Victor <bozhko.v.n@yandex.ru>
2021-09-19 19:53:15 +0300

    Added example.txt & modified SelenideFilesTest

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -1,6 +1,8 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import org.checkerframework.checker.units.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -9,14 +11,16 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideFilesTest {
 
     @Test
     void downloadFileTest() throws Exception {
-        Selenide.open("https://github.com/selenide/selenide-appium/blob/master/README.md");
+        open("https://github.com/selenide/selenide-appium/blob/master/README.md");
         File download = $(byLinkText("Raw")).download();
         String result;
         try (InputStream is = new FileInputStream(download)) {
@@ -25,7 +29,14 @@ public class SelenideFilesTest {
         assertThat(result).contains("Selenide adaptor for Appium framework");
     }
 
-    void uploadFileTest() {
-
+        @Test
+    void uploadFileTest1() {
+        open("http://file.karelia.ru/");
+        $("input[type='file']").uploadFromClasspath("example.txt");
+        $("#file_submit").click();
+        $("#fileQueue")
+                .shouldHave(text("example.txt"));
     }
+
+
 }
