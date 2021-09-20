cd458de9948f489dcc16037d5880539868a2997e
Victor <bozhko.v.n@yandex.ru>
2021-09-20 12:58:35 +0300

    Added TestBase & modified SelenideFilesTest (downloadPDFFileTest)

--- src/test/java/guru/qa/SelenideFilesTest.java
+++ src/test/java/guru/qa/SelenideFilesTest.java
@@ -1,8 +1,12 @@
 package guru.qa;
 
+import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.xlstest.XLS;
 import org.checkerframework.checker.units.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,12 +17,13 @@ import java.nio.charset.StandardCharsets;
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SelenideFilesTest {
+public class SelenideFilesTest extends TestBase {
 
-    @Test
+        @Test
     void downloadFileTest() throws Exception {
         open("https://github.com/selenide/selenide-appium/blob/master/README.md");
         File download = $(byLinkText("Raw")).download();
@@ -36,6 +41,14 @@ public class SelenideFilesTest {
         $("#file_submit").click();
         $("#fileQueue")
                 .shouldHave(text("example.txt"));
+    }
+
+        @Test
+    void downloadPDFFileTest() throws Exception {
+        open("https://амтэк35.рф/sveden/dokument/");
+        File download = $(byText("Устав")).download();
+        PDF parsed = new PDF(download);
+        assertThat(parsed.author).contains("user");
     }
 
 
